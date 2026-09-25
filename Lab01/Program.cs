4e96ad59541e366241868ae8c53f0715db315f0b
vova5148-byte <vovam5148@gmail.com>
2026-09-25 09:28:56 +0300

    Lab01: variables and interpolation

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string myName = "Мягков Владимир";
+string groupName = "ИСП-254";
+int courseNumber = 2;
+double averageGrade = 4.6;
+bool isBudget = true;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");
