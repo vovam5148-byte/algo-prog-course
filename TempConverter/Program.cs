b7b75f0afe774c3545e20a489dd87b9cfd28fa3a
vovam5148-byte <vovam5148@gmail.com>
2026-09-26 17:52:46 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,8 @@
+﻿
+double celsius = 30.5;
+double F = 45;
+const double K = 634;
+
+Console.WriteLine($"Температура по Фаренгейту: {celsius * 9/5 + F}");
+Console.WriteLine($"Температура по Кельвину: {celsius + K}");
+Console.WriteLine($"{celsius}°C = {celsius * 9 / 5 + F}°F = {(int)((celsius + K) * 100) / 100.0}K");
